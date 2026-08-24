a636c6da95d73d62721eae659c9d042621116fe5
sreenivasreddy227-ops <sreenivasreddy227@gmail.com>
2026-08-24 21:05:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Bouncing Ball Animation Started!");
+const button = document.getElementById("powerButton");
+const fighter = document.querySelector(".fighter");
+const aura = document.querySelector(".aura");
+
+button.addEventListener("click", function () {
+
+    fighter.style.transform = "translateX(-50%) scale(1.15)";
+    aura.style.background = "rgba(255, 80, 0, 0.5)";
+
+    button.innerHTML = "🔥 POWER MAX 🔥";
+
+    setTimeout(function () {
+
+        fighter.style.transform = "translateX(-50%) scale(1)";
+        aura.style.background = "rgba(120, 0, 255, 0.2)";
+
+        button.innerHTML = "⚡ POWER UP ⚡";
+
+    }, 2000);
+
+});
